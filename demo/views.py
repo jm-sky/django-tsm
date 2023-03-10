1e67280b1a25ab9cc02fd9cb2b9646abb2b10b3c
Jan Madeyski <jan.madeyski@gmail.com>
2023-03-10 15:32:20 +0100

    albums route

--- demo/views.py
+++ demo/views.py
@@ -8,8 +8,20 @@ def index(request):
   images = Image.objects.all()
 
   return render(request, 'layouts/main.html', {
-    'title': 'Album',
-    'description': 'To jest przykładowy album ze zdjęciami. Dane idą z widoku. Nie da się tu nic zrobić na razie.',
+    'title': 'Welcome',
+    'page': 'index.html',
+    'description': 'To jest strona DEMO',
     'members': members,
+  })
+
+# Create your views here.
+def albums(request):
+  images = Image.objects.all()
+  images = images or [{ 'title': 'Test' }, { 'title': 'Mountains' }]
+
+  return render(request, 'layouts/main.html', {
+    'title': 'Album',
+    'page': 'pages/albums.html',
+    'description': 'To jest przykładowy album ze zdjęciami. Dane idą z widoku. Nie da się tu nic zrobić na razie.',
     'images': images,
   })
